6aad849a29b84fee093ccb9991a5d5329a94f10a
Hugo Trichard <hugo.trichard@gmail.com>
2019-04-08 23:54:49 +0200

    modif cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,57 +2,61 @@ import React from 'react';
 
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
+import egalite from '../assets/img/egalite.jpg';
+import conseil from '../assets/img/conseil.JPG';
+import business from '../assets/img/business.jpg';
+
 import './Cards.css';
 
 
 const Cards = () => {
     return (
-        <div>
+        <div id="cards">
             <h2>Notre mission</h2>
 
-            <Container>
+            <Container fluid={true}>
                 <Row>
 
-                    <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Formations secteur public</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Voir nos formations</Button>
-                        </Card.Body>
-                    </Card>
+                    <Col xs={12} sm={12} md={6} xl={4}>
+                        <Card className="nocard">
+                            <Card.Img variant="top" src={egalite} />
+                            <Card.Body>
+                                <Card.Title>Formations secteur public</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Voir nos formations</Button>
+                            </Card.Body>
+                        </Card>
                     </Col>
 
-                    <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Formations secteur privé</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Voir nos formations</Button>
-                        </Card.Body>
-                    </Card>
+                    <Col xs={12} sm={12} md={6} xl={4}>
+                        <Card className="nocard">
+                            <Card.Img variant="top" src={business} />
+                            <Card.Body>
+                                <Card.Title>Formations secteur privé</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Voir nos formations</Button>
+                            </Card.Body>
+                        </Card>
                     </Col>
 
-                    <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Conseil, étude et audit</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Voir nos formations</Button>
-                        </Card.Body>
-                    </Card>
+                    <Col xs={12} sm={12} md={6} xl={4}>
+                        <Card className="nocard">
+                            <Card.Img variant="top" src={conseil} />
+                            <Card.Body>
+                                <Card.Title>Conseil, étude et audit</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Voir nos formations</Button>
+                            </Card.Body>
+                        </Card>
                     </Col>
 
                 </Row>
