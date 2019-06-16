921962c1fe1edf3d4f60f00562ab5fd3b8c261df
Hugo Trichard <hugo.trichard@gmail.com>
2019-06-16 16:51:46 +0200

    modif keyFacts

--- src/components/KeyFacts.js
+++ src/components/KeyFacts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Parallax } from 'react-parallax';
+import { Row, Col } from 'react-bootstrap';
+
 import './KeyFacts.css';
 
 const KeyFacts = () => {
@@ -11,35 +13,45 @@ const KeyFacts = () => {
                 bgImageAlt="the cat"
                 strength={600}
             >
-                <div id="parallax-content" style={{ height: '550px' }}>
+                <div id="parallax-content" style={{ height: '700px' }}>
                     <div id="key-facts">
-                        <div id="chiffres">
-                            <div className="fact">
-                                <h5>600</h5>
-                                <div style={{ height: '3px', width: '70%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
-                                <p>Stagiaires <br/>par an</p>
-                            </div>
-                            <div className="fact">
-                                <h5>50</h5>
-                                <div style={{ height: '3px', width: '70%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
-                                <p>Formations <br/> par an</p>
-                            </div>
-                            <div className="fact">
-                                <h5>100</h5>
-                                <div style={{ height: '3px', width: '70%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
-                                <p>Jours de formations <br/>par an</p>
-                            </div>
-                            <div className="fact">
-                                <h5>12</h5>
-                                <div style={{ height: '3px', width: '70%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
-                                <p>Effectif moyen <br/>par formation</p>
-                            </div>
-                            <div className="fact">
-                                <h5>98%</h5>
-                                <div style={{ height: '3px', width: '70%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
-                                <p>Taux <br/>de satisfaction</p>
-                            </div>
-                        </div>
+                        <Row>
+                            <Col md={{ span: 2, offset: 1 }} sm={4}>
+                                <div className="fact">
+                                    <h5>600</h5>
+                                    <div style={{ height: '3px', width: '90%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
+                                    <p>Stagiaires <br/>par an</p>
+                                </div>
+                            </Col>
+                            <Col md={2} sm={4}>
+                                <div className="fact">
+                                    <h5>50</h5>
+                                    <div style={{ height: '3px', width: '90%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
+                                    <p>Formations <br/> par an</p>
+                                </div>
+                            </Col>
+                            <Col md={2} sm={4}>
+                                <div className="fact">
+                                    <h5>100</h5>
+                                    <div style={{ height: '3px', width: '90%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
+                                    <p>Jours de formations <br/>par an</p>
+                                </div>
+                            </Col>
+                            <Col md={2} sm={4}>
+                                <div className="fact">
+                                    <h5>12</h5>
+                                    <div style={{ height: '3px', width: '90%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
+                                    <p>Effectif moyen <br/>par formation</p>
+                                </div>
+                            </Col>
+                            <Col md={2} sm={4}>
+                                <div className="fact">
+                                    <h5>98%</h5>
+                                    <div style={{ height: '3px', width: '90%', backgroundColor: 'darkcyan', margin: '15px 0' }} />
+                                    <p>Taux <br/>de satisfaction</p>
+                                </div>
+                            </Col>
+                        </Row>
                     </div>
                 </div>
             </Parallax>
